b6feeb4a4362b720dc23dd7a4394806835fd3ab2
Erin Claudio <eclaudio@gmail.com>
2025-02-11 13:58:38 -0500

    first commit

---
+++ shiny-spoon/gameday-ios/gameday-ios/SceneDelegate.swift
@@ -0,0 +1,21 @@
+import Turbo
+import UIKit
+
+private let rootURL = URL(string: "http://localhost:3000/")!
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    let pathConfiguration = PathConfiguration(sources: [
+        .server(rootURL.appending(path: "turbo/ios/path_configuration.json"))
+    ])
+
+    private lazy var navigator = TurboNavigator(pathConfiguration: pathConfiguration)
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+
+        window!.rootViewController = navigator.rootViewController
+        navigator.route(rootURL)
+    }
+}
